f7e147cdf8b736c7f975885e0d11b93a360e3443
Spencer Schien <spencer.schien@gmail.com>
2020-03-16 20:23:01 -0500

    Adding script from post

---
+++ shiny_app/ui.R
@@ -0,0 +1,28 @@
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("How much will you save?"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            numericInput("loan_amount", "Loan Amount:", 150000),
+            numericInput("annual_rate", "Annual Rate:", 0.04),
+            numericInput("term", "Term (months):", 360),
+            numericInput("xtr1", "Extra monthly payment:", 0, step = 50),
+            dateInput("start_date", "First Payment Date", format = "yyyy-mm-dd" ),
+            actionButton("calculate", "Calculate"),
+            downloadButton("report", "Download Report")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            textOutput("savings"),
+            dataTableOutput("schedule")
+        )
+    )
+))
